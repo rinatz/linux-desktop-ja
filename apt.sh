bde82f3ea45608abe702abb374757309ab06588c
Kenichiro IDA <ida.kenichiro@gmail.com>
2019-03-28 14:47:13 +0900

    Provision with Ansible

--- apt.sh
+++ apt.sh
@@ -4,7 +4,10 @@ set -eux
 function main() {
   sed -i 's,http://\(.*\.\)\?archive.ubuntu.com,http://jp.archive.ubuntu.com,g' /etc/apt/sources.list
   apt-get update
-  apt-get install -y linux-headers-$(uname -r) build-essential dkms openssh-server curl
+  apt-get install -y linux-headers-$(uname -r) build-essential dkms openssh-server curl software-properties-common
+  add-apt-repository -y ppa:ansible/ansible
+  apt-get update
+  apt-get install -y ansible
 }
 
 main "$@"
